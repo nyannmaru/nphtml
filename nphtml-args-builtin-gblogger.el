9cd533276c10fbfa739de170fde29ce46ec328cd
Yuto Sato <nyannmaru.project0214@gmail.com>
2022-12-18 01:28:17 +0900

    remove some errors

--- nphtml-args-builtin-gblogger.el
+++ nphtml-args-builtin-gblogger.el
@@ -8,22 +8,22 @@
 	(let ((archUrls "<$BlogArchiveURL$>") (archNames "<$BlogArchiveName$>")
 				(prevTitle "<$BlogPreviousItemTitle$>") (prevLink "<$BlogItemPermalinkURL$>"))
 		;;crucially important tags
-		`((:type blocky :tag Blogger  :alias gbBlogger)
+		`((:type blocky :tag Blogger  :alias gb-blogger)
 			
-			(:type blocky :tag BloggerArchives :alias gbArchives
+			(:type blocky :tag BloggerArchives :alias gb-archives
 						 :nest ((:tag li :type inline
 										:nest ((:tag a :attrs (href ,archUrls) :para ,archNames :type inline)))))
 
 			;;condition case like tags
-			(:type blocky :tag ItemPage :alias gbCondItempage)
-			(:type blocky :tag MainPage :alias gbCondMainpage)
-			(:type blocky :tag ArchivePage :alias gbCondArchivepage)
-			(:type blocky :tag MainOrArchivePage :alias gbCondMainorarch)
-			(:type blocky :tag BlogItemCommentsEnabled :alias gbCondCenabled)
+			(:type blocky :tag ItemPage :alias gbCond-itempage)
+			(:type blocky :tag MainPage :alias gbCond-mainpage)
+			(:type blocky :tag ArchivePage :alias gbCond-archivepage)
+			(:type blocky :tag MainOrArchivePage :alias gbCond-mainorarch)
+			(:type blocky :tag BlogItemCommentsEnabled :alias gbCond-cenabled)
 
 			;;templates
-			(:type blocky :tag BlogItemTitle :nest (BlogItemUrl) :alias gbItemtitle)
-			(:type inline :tag BlogItemUrl   :nest ((:tag a :attrs (href <$BlogItemUrl$>))) :hidden t)
+			(:type blocky :tag BlogItemTitle :nest (BlogItemUrl) :alias gb-itemtitle)
+			(:type inline :tag BlogItemUrl   :nest ((:tag a :type inline :attrs (href <$BlogItemUrl$>))) :hidden t)
 			(:type blocky :tag BloggerPreviousItems :alias gbPrevs
 						 :nest ((:tag li :type inline
 									:nest ((:tag a :attrs (href ,prevLink) :para ,prevTitle :type inline)))))
